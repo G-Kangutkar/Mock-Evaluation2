1d6177961969a1d106494848146dee3daa8e7017
G-Kangutkar <kangutkargauri9205@gmail.com>
2025-11-23 20:20:48 +0530

    done Q1

--- adv-js/part-A/index.js
+++ adv-js/part-A/index.js
@@ -3,17 +3,21 @@ let arr1 = [10, 20, 30];
 let arr2 = [40, 50];
 const combined = [...arr1,...arr2]
 console.log(combined);
+////////////////////////////////////////////
 
 let person = { name: "Venu", age: 25 };
 let extra = { city: "Bengaluru" };
 const profile = {...person,...extra}
 console.log(profile)
+//////////////////////////////////////////
 
-const sumAll=(...num)=>{i=>{
-    return i+num
-    
-},0}
-console.log(sumAll(1, 2, 3, 4))
+function sumAll(...num){
+    let t=0;
+    for(n of num){ t+=n};
+    console.log(t)
+}
+sumAll(1, 2, 3, 4)
+//////////////////////////////////////
 
 let numbers = [10, 20, 30, 40, 50];
 const [a,,,,]=numbers
@@ -21,6 +25,7 @@ console.log(a)
 
 const remaining = (...num)=>{return num}
 console.log(remaining(numbers))
+///////////////////////////////////////////
 
 let user = {
   name: "Alice",
@@ -37,9 +42,11 @@ const {
     }
 }=user
 console.log(city,lat,lng)
+//////////////////////////////////////////
 
 const multiply = (a,b)=> a*b;
 console.log(multiply(3,4))
+///////////////////////////////////////////////
 
 let emp = {
   name: "Prakash",
